b538d6466d659a23b1024b9d210ed35c2154beca
nin0ux <nils@galloux.net>
2024-12-12 21:59:17 +0100

    Integrate progress_timer for performance measurement in Day template and remove the test part for now

--- templates/day_template.rs
+++ templates/day_template.rs
@@ -1,3 +1,5 @@
+use progress_timer::time_function;
+
 fn part1(input: &str) -> usize {
     0
 }
@@ -11,23 +13,8 @@ fn main() {
 
     let input = aoc_utils::get_input_for_day(is_test);
 
-    println!("Part 1: {}", part1(&input));
-    println!("Part 2: {}", part2(&input));
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    const TEST_INPUT: &str = "";
-
-    #[test]
-    fn test_part1() {
-        assert_eq!(part1(TEST_INPUT), 0);
-    }
-
-    #[test]
-    fn test_part2() {
-        assert_eq!(part2(TEST_INPUT), 0);
-    }
+    let result_part_1 = time_function("Part 1", 5, || part1(&input));
+    println!("Part 1: {}", result_part_1);
+    let result_part_2 = time_function("Part 2", 5, || part2(&input));
+    println!("Part 2: {}", result_part_2);
 }
